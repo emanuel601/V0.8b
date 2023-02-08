fdebebf871085994ccdaeb1ad81eae4436b6e816
emanuel601 <eortizkovalek@gmail.com>
2023-02-08 13:12:53 -0300

    version 2.8

--- imagesearch.py
+++ imagesearch.py
@@ -5,9 +5,9 @@ from db_definitions import db, CarBrand, CarModel, ImageDataBase, app
 
 # you can provide API key and CX using arguments,
 # or you can set environment variables: GCS_DEVELOPER_KEY, GCS_CX
-
-gis = GoogleImagesSearch(os.getenv('API-KEY'), os.getenv('API-KEY-2'))
 key = os.getenv('API-KEY')
+gis = GoogleImagesSearch(key, os.getenv('API-KEY-2'))
+
 
 def image_search(q_image):
 
